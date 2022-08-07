fd0729a435fc2e7e7dc6640430114209e9a48a6c
YoungChulShin <go1323@gmail.com>
2022-08-07 23:54:55 +0900

    2-6 장

--- book_kotlin_cook_book/src/main/kotlin/ch02/2_6.kt
+++ book_kotlin_cook_book/src/main/kotlin/ch02/2_6.kt
@@ -7,6 +7,14 @@ fun Long.pow(x: Int) = toDouble().pow(x).toLong()
 
 infix fun Int.`^`(x: Int) = toDouble().pow(x).toInt()
 
+infix fun <A, B> A.to(that: B): Pair<A, B> = Pair(this, that)
+
 fun main() {
     println(2 `^` 2)
+
+    val pair = "a" to 1
+    println(pair)
+    val pair2 = "b" to 2
+    val pair3 = Pair("c", 3)
+    mapOf(pair)
 }
