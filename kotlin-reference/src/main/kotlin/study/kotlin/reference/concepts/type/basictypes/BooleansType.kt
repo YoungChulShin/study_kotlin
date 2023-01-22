c86dbd21d143f5c6d7052ef575ba88029cdf3f7e
YoungChulShin <go1323@gmail.com>
2023-01-23 00:38:42 +0900

    Booleans

---
+++ kotlin-reference/src/main/kotlin/study/kotlin/reference/concepts/type/basictypes/BooleansType.kt
@@ -0,0 +1,16 @@
+package study.kotlin.reference.concepts.type.basictypes
+
+fun main() {
+
+    // true, false를 가진다
+    val a: Boolean = true
+    println(a)
+    val b = false
+    println(b)
+
+    // nullable은 null을 가질 수 있다
+    val c: Boolean? = null
+    println(c)
+
+
+}
