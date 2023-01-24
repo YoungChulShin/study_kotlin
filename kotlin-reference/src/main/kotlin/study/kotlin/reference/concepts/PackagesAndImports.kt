d4a5e0b00944fad14893e9113b3b0c20b30763da
YoungChulShin <go1323@gmail.com>
2023-01-24 19:49:43 +0900

    package and import

---
+++ kotlin-reference/src/main/kotlin/study/kotlin/reference/concepts/PackagesAndImports.kt
@@ -0,0 +1,12 @@
+package study.kotlin.reference.concepts
+
+fun main() {
+    // 패키지를 지정하지 않으면 이름이 없는 default package가 지정된다
+
+    // 일부 패키지는 import 없이 기본적으로 적용된다
+    // println() (kotlin.io)을 사용할 때, 별도의 import 없이도 사용가능한 것은 이 때문이다.
+
+    // kotlin.*
+
+}
+
